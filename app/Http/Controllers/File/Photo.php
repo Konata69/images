b4d03376ae5056327ed6554f94e3ad9e652b412b
Konata69 <shaykinspicewolf@gmail.com>
2019-03-29 17:58:27 +0400

    feat(image_upload): загрузка изображений по ссылке
    Частичный импорт класса работы с файлами изображений

---
+++ app/Http/Controllers/File/Photo.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\File;
+
+use App\Http\Controllers\File;
+use App\Http\Controllers\Controller;
+use Exception;
+use Intervention\Image\Facades\Image;
+
+class Photo extends Controller
+{
+    use File\Traits\Processing;
+
+    /**
+     * Сохраняем файл
+     *
+     * @param string $source ссылка на изображение
+     * @param string $target путь к изображению
+     *
+     * @return bool если файл скачан true
+     */
+    public function downloadFile($source, $target)
+    {
+        $curl = curl_init();
+        $file = fopen($target, 'wb');
+
+        curl_setopt($curl, CURLOPT_FILE, $file);
+        curl_setopt($curl, CURLOPT_URL, $source);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
+        $error = curl_error($curl);
+
+        curl_exec($curl);
+        curl_close($curl);
+        fclose($file);
+
+        return empty($error);
+    }
+
+    /**
+     * Сохраняем обработанное изображение на диск
+     *
+     * @param Image $img экземпляр фасада Image с конкретным изображением
+     * @param array $path массив путей для изображения
+     *
+     * @return string путь к сохраненному изображению
+     */
+    public function savePhoto(Image $img, array $path): string
+    {
+        $this->saveImage($img, $path['base_dir'] . $path['name']);
+
+        return $path['url'] . $path['name'];
+    }
+
+    /**
+     * Сохраняем фотографию по ссылке во временный файл
+     *
+     * @param string $src ссылка на изображение
+     * @param string $path путь сохранения файла
+     *
+     * @return mixed путь ко временному файлу если загрузка изображения удалась или false
+     */
+    public function tempPhotoCreate($src, $path)
+    {
+        $path = public_path() . $path . '/temp/';
+        $name = uniqid() . '.jpg';
+
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+
+        try {
+            $res = $this->downloadFile($src, $path . $name);
+        } catch (Exception $e) {
+            $res = null;
+        }
+
+        if ($res) {
+            return $path . $name;
+        }
+
+        return false;
+    }
+
+    /**
+     * Удаляем временное изображение
+     *
+     * @param string $src путь к файлу изображения на диске
+     *
+     * @return bool
+     */
+    public function tempPhotoRemove($src)
+    {
+        $result = false;
+
+        if (file_exists($src)) {
+            $result = unlink($src);
+        }
+
+        return $result;
+    }
+}
