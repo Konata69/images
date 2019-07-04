44ccd96aed54cce4093c88d09ac0e07d56774594
Konata69 <shaykinspicewolf@gmail.com>
2019-07-04 13:34:06 +0400

    feat(img): разделить роуты по группам авто и фотобанк

--- routes/api.php
+++ routes/api.php
@@ -18,20 +18,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'auth.basic'], function () {
-    //TODO Вынести роуты в группу работы с фотобанком
+    Route::prefix('/image')->group(function () {
+        Route::prefix('/photobank')->group(function () {
+            // загрузить изображение в фотобанк по ссылке в сервис
+            Route::post('/load', 'ImageController@loadAction')->name('image-load-action');
+            // заблокировать изображение по ссылке
+            Route::post('/block', 'ImageController@blockAction')->name('image-block-action');
+            // получить список изображений по урлу (списку урлов)
+            Route::post('/by-url', 'ImageController@byUrlView')->name('image-by-url-view');
+            // получить список изображений по хешу (списку хешей)
+            Route::post('/by-hash', 'ImageController@byHashView')->name('image-by-hash-view');
+        });
 
-    // загрузить изображение в фотобанк по ссылке в сервис
-    Route::post('/image/load', 'ImageController@loadAction')->name('image-load-action');
-    // заблокировать изображение по ссылке
-    Route::post('/image/block', 'ImageController@blockAction')->name('image-block-action');
-    // получить список изображений по урлу (списку урлов)
-    Route::post('/image/by-url', 'ImageController@byUrlView')->name('image-by-url-view');
-    // получить список изображений по хешу (списку хешей)
-    Route::post('/image/by-hash', 'ImageController@byHashView')->name('image-by-hash-view');
-
-    //работа с индивидуальными изображениями авто
-    Route::prefix('/image/auto')->group(function () {
-        // загрузить индивидуальное изображение автомобиля в сервис
-        Route::post('/load', 'ImageController@loadAutoAction')->name('image-auto-load-action');
+        //работа с индивидуальными изображениями авто
+        Route::prefix('/auto')->group(function () {
+            // загрузить индивидуальное изображение автомобиля в сервис
+            Route::post('/load', 'ImageController@loadAutoAction')->name('image-auto-load-action');
+        });
     });
 });
