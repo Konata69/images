a5bdab9c07781c71393a641c99587c5a2229b8ba
Konata69 <shaykinspicewolf@gmail.com>
2019-10-01 20:15:37 +0400

    feat(calls): метод api для записи файла звонка
    - в процессе

---
+++ app/Http/Controllers/Calltracking/RecordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Calltracking;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RecordController extends Controller
+{
+    public function store(Request $request)
+    {
+        //TODO Сохранить файл записи звонка в фс и бд
+    }
+}
